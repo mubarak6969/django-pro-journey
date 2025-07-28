69f6b23b13e02fe07196726d2f88e6d2c0da1113
mubarak6969 <mubarak7842a@gmail.com>
2025-07-28 18:04:18 +0530

    final part

--- tracker/views.py
+++ tracker/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import MoodEntry
 
-# Existing view
 def mood_entry_view(request):
     if request.method == 'POST':
         mood = request.POST.get('mood')
         note = request.POST.get('note')
         MoodEntry.objects.create(mood=mood, note=note)
-        return redirect('mood-form')
-    
+        messages.success(request, "Mood entry saved!")
+        return redirect('mood-form')  # or use your named URL
+
     return render(request, 'tracker/mood_form.html')
 
-# 🔹 New view to list all entries
+
 def mood_history_view(request):
-    mood_filter = request.GET.get('mood')  # Get ?mood= from URL
+    mood_filter = request.GET.get('mood')  # get ?mood= from URL
     if mood_filter:
         entries = MoodEntry.objects.filter(mood=mood_filter).order_by('-date')
     else:
