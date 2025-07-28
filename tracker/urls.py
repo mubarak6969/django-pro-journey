ea698344ba354e36fdb8ca6f83eee21bd16bb87f
mubarak6969 <mubarak7842a@gmail.com>
2025-07-28 22:49:31 +0530

    Fix import error for mood_history_view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import mood_entry_view, mood_history_view
+from .views import mood_entry_view
 
 urlpatterns = [
     path('', mood_entry_view, name='mood-form'),
